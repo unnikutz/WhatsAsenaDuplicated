63ccb44d2bd70b6bf15b1780de4f7ffe88bfc746
unnikutz <78098073+unnikutz@users.noreply.github.com>
2021-01-31 00:25:51 +0530

    Update deepai.js

--- plugins/deepai.js
+++ plugins/deepai.js
@@ -13,13 +13,13 @@ deepai.setApiKey('4ec4c7f4-63cd-457f-b244-7e12bba7ebde'); // Quickstart API Key
 const Language = require('../language'); 
 const Lang = Language.getString('deepai'); // Language support
 
-Asena.addCommand({pattern: 'deepai', fromMe: true, deleteCommand: false, desc: Lang.DEEPAI_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'deepai', fromMe: false, deleteCommand: false, desc: Lang.DEEPAI_DESC}, (async (message, match) => {
 
     await message.sendMessage('💻 Usage: *.moodai <text>*\nℹ️ Desc: 🇹🇷 Yazdığınız yazıdan ruh halinizi bulur.\n🇬🇧 It finds your mood from the article you wrote.\n\n💻 Usage: *.colorai <link>*\nℹ️ Desc: 🇹🇷 Siyah beyaz fotoğrafları renklendirir.\n🇬🇧 It colorize bw photos.\n\n💻 Usage: *.superai <image_link>*\nℹ️ Desc: 🇹🇷 Fotoğrafın kalitesini yapay zeka ile arttırır.\n🇬🇧 Improves the quality of photos with Neural AI.\n\n💻 Usage: *.waifuai <image_link>*\nℹ️ Desc: 🇹🇷 Fotoğrafların renk paletlerini yapay zeka ile birleştirir.\n🇬🇧 Combines the color palettes of photos with artificial intelligence.\n\n💻 Usage: *.dreamai <image_link>*\nℹ️ Desc: 🇹🇷 Fotoğrafa deepdream efekti uygular.\n🇬🇧 Applies deepdream effect to the photo.\n\n💻 Usage: *.neuraltalkai <image_link>*\nℹ️ Desc: 🇹🇷 Fotoğrafki olan şeyi yapay zeka ile açıklar.\n🇬🇧 Explain the phenomenon in the photo with artificial intelligence.\n\n💻 Usage: *.ttiai <text>*\nℹ️ Desc: 🇹🇷 Yazıyı resme dönüştürür.\n🇬🇧 Converts text to a picture. (Text-to-Image)\n\n💻 Usage: *.toonai <image_link>*\nℹ️ Desc: 🇹🇷 Fotoğraftaki yüzü çizgi film karakterine çevirir.\n🇬🇧 Turns the face in the photo into a cartoon character.\n\n⚠️ 🇹🇷 *Bütün bu yapay zeka araçlarını derin öğrenme ile çalışır. Ne kadar fazla kullanırsanız o kadar fazla bilgiyi depolar.* ```Sadece ingilizce karakter kullanın!```\n\n⚠️ 🇬🇧 *All the tools here work with deep learning. The more you use it, the more information it stores.* ```Use only english characters!```');
 
 }));
 
-Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
     var resp = await deepai.callStandardApi("sentiment-analysis", {
@@ -31,7 +31,7 @@ Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: true, deleteCommand: false, d
 
 }));
 
-Asena.addCommand({pattern: 'colorai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'colorai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("colorizer", {
@@ -43,7 +43,7 @@ Asena.addCommand({pattern: 'colorai ?(.*)', fromMe: true, deleteCommand: false, 
 
 }));
 
-Asena.addCommand({pattern: 'superai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'superai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("torch-srgan", {
@@ -55,7 +55,7 @@ Asena.addCommand({pattern: 'superai ?(.*)', fromMe: true, deleteCommand: false, 
 
 }));
 
-Asena.addCommand({pattern: 'waifuai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'waifuai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("waifu2x", {
@@ -67,7 +67,7 @@ Asena.addCommand({pattern: 'waifuai ?(.*)', fromMe: true, deleteCommand: false, 
 
 }));
 
-Asena.addCommand({pattern: 'dreamai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'dreamai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("deepdream", {
@@ -79,7 +79,7 @@ Asena.addCommand({pattern: 'dreamai ?(.*)', fromMe: true, deleteCommand: false, 
 
 }));
 
-Asena.addCommand({pattern: 'neuraltalkai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'neuraltalkai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("neuraltalk", {
@@ -91,7 +91,7 @@ Asena.addCommand({pattern: 'neuraltalkai ?(.*)', fromMe: true, deleteCommand: fa
 
 }));
 
-Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
     var resp = await deepai.callStandardApi("text2img", {
@@ -103,7 +103,7 @@ Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: true, deleteCommand: false, do
 
 }));
 
-Asena.addCommand({pattern: 'toonai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'toonai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.URL);
 
     var resp = await deepai.callStandardApi("toonify", {
