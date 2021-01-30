610e55a390b9519c382c12cf7989e7db7363c483
unnikutz <78098073+unnikutz@users.noreply.github.com>
2021-01-31 00:50:17 +0530

    Update Image.js

--- whatsasena/Image.js
+++ whatsasena/Image.js
@@ -3,10 +3,10 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAlexa - Abhinav
 */
 
-const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
+const {MessageType, Presence, MessageOptions} = require('@Abhinavdhanjay/baileys');
 const Base = require('./Base');
 const Message = require('./Message');
 const ReplyMessage = require('./ReplyMessage');
